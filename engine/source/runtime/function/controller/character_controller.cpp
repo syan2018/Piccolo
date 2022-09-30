b1cd7b8d94d52b8ffeb76bbf6b132edf4b380df3
Syan <707534106@qq.com>
2022-09-30 16:34:44 +0800

    PA03_3

--- engine/source/runtime/function/controller/character_controller.cpp
+++ engine/source/runtime/function/controller/character_controller.cpp
@@ -29,24 +29,36 @@ namespace Pilot
 
     Vector3 CharacterController::move(const Vector3& current_position, const Vector3& displacement)
     {
+        // 获取场景
         std::shared_ptr<PhysicsScene> physics_scene =
             g_runtime_global_context.m_world_manager->getCurrentActivePhysicsScene().lock();
         ASSERT(physics_scene);
 
         std::vector<PhysicsHitInfo> hits;
 
+        // 为啥要叠高Z？
+        // 取单位在场景系中Transform
         Transform world_transform = Transform(
             current_position + 0.1f * Vector3::UNIT_Z,
             Quaternion::IDENTITY,
             Vector3::UNIT_SCALE);
 
+
+        // 取位置更新向量
         Vector3 vertical_displacement   = displacement.z * Vector3::UNIT_Z;
         Vector3 horizontal_displacement = Vector3(displacement.x, displacement.y, 0.f);
 
         Vector3 vertical_direction   = vertical_displacement.normalisedCopy();
         Vector3 horizontal_direction = horizontal_displacement.normalisedCopy();
 
+
+        // 位置更新Part
+        
         Vector3 final_position = current_position;
+
+
+        // 输入角色碰撞体、角色位置、检测方向、检测距离
+        // 角色位置虚空抬了0.1，Z轴下探检测0.105用于检测地板
 
         m_is_touch_ground = physics_scene->sweep(
             m_rigidbody_shape,
@@ -59,14 +71,21 @@ namespace Pilot
         
         world_transform.m_position -= 0.1f * Vector3::UNIT_Z;
 
+
+        // 垂向触地
         // vertical pass
-        if (physics_scene->sweep(
+
+        bool has_vertical_hits = physics_scene->sweep(
             m_rigidbody_shape,
             world_transform.getMatrix(),
             vertical_direction,
             vertical_displacement.length(),
-            hits))
+            hits);
+
+        // 如果触地则卡在第一个碰撞位
+        if (has_vertical_hits)
         {
+            
             final_position += hits[0].hit_distance * vertical_direction;
         }
         else
@@ -76,17 +95,31 @@ namespace Pilot
 
         hits.clear();
 
+
         // side pass
-        //if (physics_scene->sweep(
-        //    m_rigidbody_shape,
-        //    /**** [0] ****/,
-        //    /**** [1] ****/,
-        //    /**** [2] ****/,
-        //    hits))
-        //{
-        //    final_position += /**** [3] ****/;
-        //}
-        //else
+
+        bool has_horizontal_hits = physics_scene->sweep(
+            m_rigidbody_shape,
+            world_transform.getMatrix(),
+            horizontal_direction,
+            horizontal_displacement.length(),
+            hits);
+
+        if (has_horizontal_hits)
+        {
+
+            Pilot::PhysicsHitInfo hitInfo = hits[0];
+
+            // final_position += hitInfo.hit_distance * horizontal_direction;
+
+            final_position += 
+                horizontal_displacement.length() 
+                * (horizontal_direction - hitInfo.hit_normal);
+            
+
+
+        }
+        else
         {
             final_position += horizontal_displacement;
         }
